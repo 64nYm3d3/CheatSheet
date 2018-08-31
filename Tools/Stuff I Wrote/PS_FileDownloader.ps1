de6eb55ffdd520871c024ca3e9174a69ace64ace
64nYm3d3 <brentwong021@gmail.com>
2018-08-31 11:19:45 -0500

    Update PS_FileDownloader.ps1

--- Tools/Stuff I Wrote/PS_FileDownloader.ps1
+++ Tools/Stuff I Wrote/PS_FileDownloader.ps1
@@ -1,10 +1,14 @@
 
 
 <#
-I would love to say I made this for a ctf but I'd be lying. This was made because I was too lazy to download and install curl on my Win10 machine for a while. It was made as a way to download multiple images from an image gallery I was looking at. 
+I would love to say I made this for a ctf but I'd be lying. This was made because
+I was too lazy to download and install curl on my Win10 machine for a while. It 
+was made as a way to download multiple images from an image gallery I was looking at. 
 
 
-Might need some modification, when user info is passed to the variable website, there might be an issue if certain characters come up. I threw in a lazy fix in the main function but who knows if it will work.
+Might need some modification, when user info is passed to the variable website, 
+there might be an issue if certain characters come up. I threw in a lazy fix in
+the main function but who knows if it will work.
 #>
 
 $filetype = $null
